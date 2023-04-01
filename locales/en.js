fbd481d7854dd197f871798b7c9e26f934e92022
SAQI <88505820+SaqiDeveloper@users.noreply.github.com>
2023-04-02 03:25:15 +0500

    Update en.js

--- locales/en.js
+++ locales/en.js
@@ -1,7 +1,7 @@
 {
 	"welcome": "Welcome to my API!",
 	"error": {
-		"notFound": "The requested resource was not found changes.",
+		"notFound": "The requested resource was not found jenkins testing.",
 		"serverError": "An unexpected error occurred. Please try again later."
 	},
 	"The requested resource was not found.": "The requested resource was not found."
