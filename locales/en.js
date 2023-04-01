71585a4c23b4fa3c57969fcb9b43c421a7e11bdc
SaqiDeveloper <dynamicdeveloper434@gmail.com>
2023-04-02 02:47:35 +0500

    push

--- locales/en.js
+++ locales/en.js
@@ -1,7 +1,7 @@
 {
 	"welcome": "Welcome to my API!",
 	"error": {
-		"notFound": "The requested resource was not found saqi.",
+		"notFound": "The requested resource was not found saqi how are u.",
 		"serverError": "An unexpected error occurred. Please try again later."
 	},
 	"The requested resource was not found.": "The requested resource was not found."
