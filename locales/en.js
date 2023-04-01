84834837e0a3ebaf2122d686a40707607eac81fe
SAQI <88505820+SaqiDeveloper@users.noreply.github.com>
2023-04-02 03:22:49 +0500

    Update en.js

--- locales/en.js
+++ locales/en.js
@@ -1,7 +1,7 @@
 {
 	"welcome": "Welcome to my API!",
 	"error": {
-		"notFound": "The requested resource was not found Hello.",
+		"notFound": "The requested resource was not found changes.",
 		"serverError": "An unexpected error occurred. Please try again later."
 	},
 	"The requested resource was not found.": "The requested resource was not found."
